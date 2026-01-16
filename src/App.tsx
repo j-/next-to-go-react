0d0285fa3aeebb849ce9d8fb9063d6ef4ee7bc01
j- <j@skeoh.com>
2026-01-16 14:35:05 +1100

    Add response schema

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,36 @@ import { getNextRacesOptions } from './api';
 
 export const App: FC = () => {
   const {
+    error,
     data,
   } = useQuery(
     getNextRacesOptions(),
   );
 
+  if (error) {
+    return (
+      <div>
+        <h1>Request failed</h1>
+        <pre>{String(error)}</pre>
+      </div>
+    );
+  }
+
   return (
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div>
+      <ol>
+        {data?.map((race) => (
+          <li key={race.race_id}>
+            <strong>{race.meeting_name} R{race.race_number}</strong>
+            <br />
+            <em>{race.race_id}</em>
+          </li>
+        ))}
+      </ol>
+
+      <hr />
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,9 @@
 import { queryOptions } from '@tanstack/react-query';
+import z from 'zod';
+
+export const CATEGORY_ID_GREYHOUND = '9daef0d7-bf3c-4f50-921d-8e818c60fe61';
+export const CATEGORY_ID_HARNESS = '161d9be2-e909-4326-8c2c-35ed71fb460b';
+export const CATEGORY_ID_HORSE = '4a2788f8-e825-4d36-9894-efd4baf1cfae';
 
 export type GetNextRacesParams = {
   count: number;
@@ -11,6 +16,27 @@ export const getNextRacesDefaults = {
 export const getNextRacesQueryKey = (params = getNextRacesDefaults) => (
   ['nextraces', params]
 );
+
+// We can be more specific here and declare IDs as `z.uuid()`, however
+// the application doesn't care about the format of these IDs.
+export const raceIdSchema = z.string();
+
+// We only declare fields we care about since any mistake in correctly typing
+// fields we DON'T care about will lead to the entire payload failing to parse.
+export const raceSummarySchema = z.object({
+  race_id: raceIdSchema,
+  race_name: z.string(),
+  race_number: z.number(),
+  meeting_name: z.string(),
+  category_id: z.string(),
+});
+
+export const getNextRacesSchema = z.object({
+  data: z.object({
+    next_to_go_ids: z.array(raceIdSchema),
+    race_summaries: z.record(raceIdSchema, raceSummarySchema),
+  }),
+});
 
 export const getNextRacesOptions = (params = getNextRacesDefaults) => (
   queryOptions({
@@ -26,7 +52,19 @@ export const getNextRacesOptions = (params = getNextRacesDefaults) => (
       const res = await fetch(url, { signal });
       if (!res.ok) throw new Error('Response was not OK');
 
-      return res.json();
+      const payload = await res.json();
+      return getNextRacesSchema.parse(payload);
     },
+
+    // Denormalizes the payload so that we just get a
+    // simple array of races that are next to jump.
+    select: ({ data }) => {
+      return data.next_to_go_ids.map((raceId) => {
+        return data.race_summaries[raceId];
+      }).filter((race) => race != null);
+    },
+
+    // Invalidate and refresh every 30 seconds.
+    refetchInterval: 30_000,
   })
 );
