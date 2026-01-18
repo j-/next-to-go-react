1043aa129725377ffac4acfb9cd10e943277b4cd
j- <j@skeoh.com>
2026-01-18 12:14:35 +1100

    Add race summary placeholder component

--- src/NextToGo.tsx
+++ src/NextToGo.tsx
@@ -1,5 +1,6 @@
 import { useMemo, type FC } from 'react';
 import type { RaceSummary } from './api';
+import { RaceSummaryPlaceholder } from './RaceSummaryPlaceholder';
 import { RaceSummaryView } from './RaceSummaryView';
 
 export type NextToGoProps = {
@@ -13,17 +14,33 @@ export const NextToGo: FC<NextToGoProps> = ({
   limit,
   now,
 }) => {
-  const slice = useMemo(() => {
-    return nextToGo.slice(0, limit);
-  }, [nextToGo, limit]);
+  /**
+   * Renders a fixed length array (where length = `limit`) of race summary view
+   * components, filling blank spaces with placeholders such that if the list is
+   * still loading or the number of races is less than the limit then the total
+   * number of list items will still be `limit` but filled with placeholders
+   * instead.
+   */
+  const children = useMemo(() => {
+    // Construct fixed length array.
+    return Array.from({ length: limit })
+      // Fill with next-to-go races.
+      .map((_, i) => nextToGo[i])
+      // Always render X summary views + placeholders.
+      .map((race, i) => (
+        race ?
+          <li key={race.race_id}>
+            <RaceSummaryView race={race} now={now} />
+          </li> :
+          <li key={`placeholder-${i}`}>
+            <RaceSummaryPlaceholder />
+          </li>
+      ));
+  }, [limit, nextToGo, now]);
 
   return (
     <ol>
-      {slice?.map((race) => (
-        <li key={race.race_id}>
-          <RaceSummaryView race={race} now={now} />
-        </li>
-      ))}
+      {children}
     </ol>
   );
 };
---
+++ src/RaceSummaryPlaceholder.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+
+export const RaceSummaryPlaceholder: FC = () => {
+  return (
+    <div>
+      <strong>&mdash;</strong>
+      <br />
+      &mdash;
+      <br />
+      &mdash;
+    </div>
+  );
+};
